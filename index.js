21bd4d1f12c50d20b05a40ba0da8b094d90408d3
Roneesh <roneesh@gmail.com>
2016-08-11 14:00:14 -0400

    beginning to update ui for mint function

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
 	var eth = web3.eth;
 	web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
 
+	var rcABI = [ { "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string", "value": "Test2RecurseCoin" } ], "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8", "value": "2" } ], "type": "function" }, { "constant": false, "inputs": [], "name": "kill", "outputs": [], "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string", "value": "😎" } ], "type": "function" }, { "constant": false, "inputs": [ { "name": "_value", "type": "uint256" } ], "name": "mint", "outputs": [], "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [], "type": "function" }, { "inputs": [ { "name": "initialSupply", "type": "uint256", "index": 0, "typeShort": "uint", "bits": "256", "displayName": "initial Supply", "template": "elements_input_uint", "value": "1000" }, { "name": "tokenName", "type": "string", "index": 1, "typeShort": "string", "bits": "", "displayName": "token Name", "template": "elements_input_string", "value": "Test2RecurseCoin" }, { "name": "decimalUnits", "type": "uint8", "index": 2, "typeShort": "uint", "bits": "8", "displayName": "decimal Units", "template": "elements_input_uint", "value": "2" }, { "name": "tokenSymbol", "type": "string", "index": 3, "typeShort": "string", "bits": "", "displayName": "token Symbol", "template": "elements_input_string", "value": "😎" } ], "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Mint", "type": "event" } ];
+	var rcAddress = '0x9656EA260b226798493c6b6b15E43E1f508AFd68'
+	var recurseCoin = eth.contract(rcABI).at(rcAddress);
+
 	$('#send-coins').on('click', function() {
 		var transaction = {
 			'from': $('#from-field').val(),
-			'to': $('#to-field').val(),
-			'value': web3.toWei(parseInt($('#amount').val(), 10), "ether"),
-			'pwd' : $('#password').val()
+			'value': web3.toWei(parseInt($('#amount').val(), 10), "ether")
 		}
 		var transactionResult = web3.eth.sendTransaction(transaction);
 		alert(transactionResult);
